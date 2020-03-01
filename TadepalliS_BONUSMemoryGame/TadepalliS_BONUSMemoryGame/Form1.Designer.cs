8c7f9b8bf6a163d42dc807ebb17bb866494f6866
SaikrishnaTadepalli <58894708+SaikrishnaTadepalli@users.noreply.github.com>
2020-03-01 18:48:48 -0500

    Add files via upload

---
+++ TadepalliS_BONUSMemoryGame/TadepalliS_BONUSMemoryGame/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace TadepalliS_BONUSMemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnBox9 = new System.Windows.Forms.Button();
+            this.btnBox8 = new System.Windows.Forms.Button();
+            this.btnBox7 = new System.Windows.Forms.Button();
+            this.btnBox6 = new System.Windows.Forms.Button();
+            this.btnBox5 = new System.Windows.Forms.Button();
+            this.btnBox4 = new System.Windows.Forms.Button();
+            this.btnBox3 = new System.Windows.Forms.Button();
+            this.btnBox2 = new System.Windows.Forms.Button();
+            this.btnBox1 = new System.Windows.Forms.Button();
+            this.btnBox0 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(9, 188);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(393, 81);
+            this.btnStart.TabIndex = 21;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnBox9
+            // 
+            this.btnBox9.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBox9.Location = new System.Drawing.Point(327, 93);
+            this.btnBox9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBox9.Name = "btnBox9";
+            this.btnBox9.Size = new System.Drawing.Size(75, 81);
+            this.btnBox9.TabIndex = 20;
+            this.btnBox9.Text = "X";
+            this.btnBox9.UseVisualStyleBackColor = true;
+            // 
+            // btnBox8
+            // 
+            this.btnBox8.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBox8.Location = new System.Drawing.Point(248, 93);
+            this.btnBox8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBox8.Name = "btnBox8";
+            this.btnBox8.Size = new System.Drawing.Size(75, 81);
+            this.btnBox8.TabIndex = 19;
+            this.btnBox8.Text = "X";
+            this.btnBox8.UseVisualStyleBackColor = true;
+            // 
+            // btnBox7
+            // 
+            this.btnBox7.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBox7.Location = new System.Drawing.Point(168, 93);
+            this.btnBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBox7.Name = "btnBox7";
+            this.btnBox7.Size = new System.Drawing.Size(75, 81);
+            this.btnBox7.TabIndex = 18;
+            this.btnBox7.Text = "X";
+            this.btnBox7.UseVisualStyleBackColor = true;
+            // 
+            // btnBox6
+            // 
+            this.btnBox6.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBox6.Location = new System.Drawing.Point(88, 93);
+            this.btnBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBox6.Name = "btnBox6";
+            this.btnBox6.Size = new System.Drawing.Size(75, 81);
+            this.btnBox6.TabIndex = 17;
+            this.btnBox6.Text = "X";
+            this.btnBox6.UseVisualStyleBackColor = true;
+            // 
+            // btnBox5
+            // 
+            this.btnBox5.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBox5.Location = new System.Drawing.Point(9, 93);
+            this.btnBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBox5.Name = "btnBox5";
+            this.btnBox5.Size = new System.Drawing.Size(75, 81);
+            this.btnBox5.TabIndex = 16;
+            this.btnBox5.Text = "X";
+            this.btnBox5.UseVisualStyleBackColor = true;
+            // 
+            // btnBox4
+            // 
+            this.btnBox4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBox4.Location = new System.Drawing.Point(327, 6);
+            this.btnBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBox4.Name = "btnBox4";
+            this.btnBox4.Size = new System.Drawing.Size(75, 81);
+            this.btnBox4.TabIndex = 15;
+            this.btnBox4.Text = "X";
+            this.btnBox4.UseVisualStyleBackColor = true;
+            // 
+            // btnBox3
+            // 
+            this.btnBox3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBox3.Location = new System.Drawing.Point(248, 6);
+            this.btnBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBox3.Name = "btnBox3";
+            this.btnBox3.Size = new System.Drawing.Size(75, 81);
+            this.btnBox3.TabIndex = 14;
+            this.btnBox3.Text = "X";
+            this.btnBox3.UseVisualStyleBackColor = true;
+            // 
+            // btnBox2
+            // 
+            this.btnBox2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBox2.Location = new System.Drawing.Point(168, 6);
+            this.btnBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBox2.Name = "btnBox2";
+            this.btnBox2.Size = new System.Drawing.Size(75, 81);
+            this.btnBox2.TabIndex = 13;
+            this.btnBox2.Text = "X";
+            this.btnBox2.UseVisualStyleBackColor = true;
+            // 
+            // btnBox1
+            // 
+            this.btnBox1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBox1.Location = new System.Drawing.Point(88, 6);
+            this.btnBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBox1.Name = "btnBox1";
+            this.btnBox1.Size = new System.Drawing.Size(75, 81);
+            this.btnBox1.TabIndex = 12;
+            this.btnBox1.Text = "X";
+            this.btnBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnBox0
+            // 
+            this.btnBox0.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBox0.Location = new System.Drawing.Point(9, 6);
+            this.btnBox0.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBox0.Name = "btnBox0";
+            this.btnBox0.Size = new System.Drawing.Size(75, 81);
+            this.btnBox0.TabIndex = 11;
+            this.btnBox0.Text = "X";
+            this.btnBox0.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(411, 275);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnBox9);
+            this.Controls.Add(this.btnBox8);
+            this.Controls.Add(this.btnBox7);
+            this.Controls.Add(this.btnBox6);
+            this.Controls.Add(this.btnBox5);
+            this.Controls.Add(this.btnBox4);
+            this.Controls.Add(this.btnBox3);
+            this.Controls.Add(this.btnBox2);
+            this.Controls.Add(this.btnBox1);
+            this.Controls.Add(this.btnBox0);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Memory Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnBox9;
+        private System.Windows.Forms.Button btnBox8;
+        private System.Windows.Forms.Button btnBox7;
+        private System.Windows.Forms.Button btnBox6;
+        private System.Windows.Forms.Button btnBox5;
+        private System.Windows.Forms.Button btnBox4;
+        private System.Windows.Forms.Button btnBox3;
+        private System.Windows.Forms.Button btnBox2;
+        private System.Windows.Forms.Button btnBox1;
+        private System.Windows.Forms.Button btnBox0;
+    }
+}
+
---
+++ TadepalliS_BONUSMemoryGame/TadepalliS_BONUSMemoryGame/Form1.cs
@@ -0,0 +1,114 @@
+﻿/************************************************************
+    PROGRAMME : BONUS Memory Game
+
+    OUTLINE : In this program the player gets to click on any 2 Buttons.
+              Each button has a hidden letter, and if the two selected
+              buttons have the same letter then they remain flipped. If 
+              there is no match, the buttons go Red for a second they 
+              go back to the X. When all letters are matched, the player wins.              
+
+    PROGRAMMER : Saikrishna Tadepalli
+
+    DATE : October 31, 2019
+ ************************************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TadepalliS_BONUSMemoryGame
+{
+    public partial class Form1 : Form
+    {
+        Button[] btnBox = new Button[10];
+        Button[] btnButtonsClicked = new Button[2];
+        string[] letterArray = new string[10];
+        int clickCount = 0;
+        int finished = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < btnBox.Length; i++)
+            {
+                btnBox[i] = (this.Controls[("btnBox" + i)] as Button);
+                btnBox[i].Click += new EventHandler(clickedButton);
+                btnBox[i].Enabled = false;
+                btnBox[i].BackColor = Color.White;
+            }
+        }
+        private void clickedButton(object sender, System.EventArgs e)
+        {
+            clickCount += 1;
+            btnButtonsClicked[1] = btnButtonsClicked[0];
+            btnButtonsClicked[0] = sender as Button;
+            btnButtonsClicked[0].Text = letterArray[Array.IndexOf(btnBox, btnButtonsClicked[0])];                        
+
+                if (clickCount % 2 == 0)
+                {
+                    if (btnButtonsClicked[0].Text == btnButtonsClicked[1].Text)
+                    {
+                        for (int i = 0; i < btnButtonsClicked.Length; i++)
+                        {
+                            btnButtonsClicked[i].BackColor = Color.LightGreen;
+                            btnButtonsClicked[i].Enabled = false;
+                            btnButtonsClicked[i] = null;
+                        }
+                        finished += 1;
+                    }
+                else
+                    {
+                        btnButtonsClicked[0].BackColor = Color.Red;
+                        btnButtonsClicked[1].BackColor = Color.Red;
+                    for (int i = 0; i < btnBox.Length; i++)
+                        btnBox[i].Enabled = false;
+                    this.Refresh();
+                        Thread.Sleep(500);
+                    for (int i = 0; i < btnBox.Length; i++)
+                        btnBox[i].Enabled = true;
+                    for (int i = 0; i < btnButtonsClicked.Length; i++)
+                        {
+                            btnButtonsClicked[i].Enabled = true;
+                            btnButtonsClicked[i].BackColor = Color.White;
+                            btnButtonsClicked[i].Text = "X";
+                        }
+                    }
+                }
+                else
+                {
+                    btnButtonsClicked[0].BackColor = Color.LightGray;
+                    btnButtonsClicked[0].Enabled = false;
+                }
+
+            if (finished == 5)
+                MessageBox.Show("Winner!","Memory Game", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < btnBox.Length; i++)
+            {
+                btnBox[i].BackColor = Color.White;
+                btnBox[i].Text = "X";
+                btnBox[i].Enabled = true;
+            }
+            finished = 0; clickCount = 0;
+            GenerateLetters();
+        }
+        private void GenerateLetters()
+        {
+            Random rand = new Random();
+            letterArray = ("A A B B C C D D E E").Split(Convert.ToChar(" ")).OrderBy(x => rand.Next()).ToArray();
+        }
+    }
+}
