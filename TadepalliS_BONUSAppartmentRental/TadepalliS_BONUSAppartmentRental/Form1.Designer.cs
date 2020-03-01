9d3ff5c76080843cd77cc263beeec716a2ae5a97
SaikrishnaTadepalli <58894708+SaikrishnaTadepalli@users.noreply.github.com>
2020-03-01 18:43:26 -0500

    Add files via upload

---
+++ TadepalliS_BONUSAppartmentRental/TadepalliS_BONUSAppartmentRental/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace TadepalliS_BONUSAppartmentRental
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAppartmentUnits = new System.Windows.Forms.Label();
+            this.txtAppartmentUnits = new System.Windows.Forms.TextBox();
+            this.txtRentAll = new System.Windows.Forms.TextBox();
+            this.lblOccupationRent = new System.Windows.Forms.Label();
+            this.txtRentIncrease = new System.Windows.Forms.TextBox();
+            this.lblIncreaseRent = new System.Windows.Forms.Label();
+            this.txtMaintainUnit = new System.Windows.Forms.TextBox();
+            this.lblMaintainUnit = new System.Windows.Forms.Label();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAppartmentUnits
+            // 
+            this.lblAppartmentUnits.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAppartmentUnits.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppartmentUnits.Location = new System.Drawing.Point(9, 7);
+            this.lblAppartmentUnits.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAppartmentUnits.Name = "lblAppartmentUnits";
+            this.lblAppartmentUnits.Size = new System.Drawing.Size(512, 27);
+            this.lblAppartmentUnits.TabIndex = 0;
+            this.lblAppartmentUnits.Text = "Enter the Number of Appartment Units:";
+            this.lblAppartmentUnits.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtAppartmentUnits
+            // 
+            this.txtAppartmentUnits.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAppartmentUnits.Location = new System.Drawing.Point(525, 7);
+            this.txtAppartmentUnits.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAppartmentUnits.Name = "txtAppartmentUnits";
+            this.txtAppartmentUnits.Size = new System.Drawing.Size(151, 27);
+            this.txtAppartmentUnits.TabIndex = 1;
+            this.txtAppartmentUnits.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtRentAll
+            // 
+            this.txtRentAll.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRentAll.Location = new System.Drawing.Point(525, 38);
+            this.txtRentAll.Margin = new System.Windows.Forms.Padding(2);
+            this.txtRentAll.Name = "txtRentAll";
+            this.txtRentAll.Size = new System.Drawing.Size(151, 27);
+            this.txtRentAll.TabIndex = 3;
+            this.txtRentAll.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblOccupationRent
+            // 
+            this.lblOccupationRent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOccupationRent.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOccupationRent.Location = new System.Drawing.Point(11, 38);
+            this.lblOccupationRent.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblOccupationRent.Name = "lblOccupationRent";
+            this.lblOccupationRent.Size = new System.Drawing.Size(510, 27);
+            this.lblOccupationRent.TabIndex = 2;
+            this.lblOccupationRent.Text = "Enter the Rent to Occupy all Units:";
+            this.lblOccupationRent.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtRentIncrease
+            // 
+            this.txtRentIncrease.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRentIncrease.Location = new System.Drawing.Point(525, 69);
+            this.txtRentIncrease.Margin = new System.Windows.Forms.Padding(2);
+            this.txtRentIncrease.Name = "txtRentIncrease";
+            this.txtRentIncrease.Size = new System.Drawing.Size(151, 27);
+            this.txtRentIncrease.TabIndex = 5;
+            this.txtRentIncrease.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblIncreaseRent
+            // 
+            this.lblIncreaseRent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblIncreaseRent.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIncreaseRent.Location = new System.Drawing.Point(11, 69);
+            this.lblIncreaseRent.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblIncreaseRent.Name = "lblIncreaseRent";
+            this.lblIncreaseRent.Size = new System.Drawing.Size(510, 27);
+            this.lblIncreaseRent.TabIndex = 4;
+            this.lblIncreaseRent.Text = "Enter the Increase in Rent that Results in  a Vacant Unit:";
+            this.lblIncreaseRent.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtMaintainUnit
+            // 
+            this.txtMaintainUnit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaintainUnit.Location = new System.Drawing.Point(525, 100);
+            this.txtMaintainUnit.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMaintainUnit.Name = "txtMaintainUnit";
+            this.txtMaintainUnit.Size = new System.Drawing.Size(151, 27);
+            this.txtMaintainUnit.TabIndex = 7;
+            this.txtMaintainUnit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblMaintainUnit
+            // 
+            this.lblMaintainUnit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMaintainUnit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaintainUnit.Location = new System.Drawing.Point(11, 100);
+            this.lblMaintainUnit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMaintainUnit.Name = "lblMaintainUnit";
+            this.lblMaintainUnit.Size = new System.Drawing.Size(510, 27);
+            this.lblMaintainUnit.TabIndex = 6;
+            this.lblMaintainUnit.Text = "Enter the Amount of Maintain a Rented Unit:";
+            this.lblMaintainUnit.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOut
+            // 
+            this.lblOut.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOut.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOut.Location = new System.Drawing.Point(9, 142);
+            this.lblOut.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(667, 75);
+            this.lblOut.TabIndex = 8;
+            this.lblOut.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(9, 231);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(512, 41);
+            this.btnCalculate.TabIndex = 9;
+            this.btnCalculate.Text = "Calculate the Number of Rented Units for Max Profit\r\n";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(525, 230);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(151, 40);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(685, 280);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.txtMaintainUnit);
+            this.Controls.Add(this.lblMaintainUnit);
+            this.Controls.Add(this.txtRentIncrease);
+            this.Controls.Add(this.lblIncreaseRent);
+            this.Controls.Add(this.txtRentAll);
+            this.Controls.Add(this.lblOccupationRent);
+            this.Controls.Add(this.txtAppartmentUnits);
+            this.Controls.Add(this.lblAppartmentUnits);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmMain";
+            this.Text = "Appartment Rental";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAppartmentUnits;
+        private System.Windows.Forms.TextBox txtAppartmentUnits;
+        private System.Windows.Forms.TextBox txtRentAll;
+        private System.Windows.Forms.Label lblOccupationRent;
+        private System.Windows.Forms.TextBox txtRentIncrease;
+        private System.Windows.Forms.Label lblIncreaseRent;
+        private System.Windows.Forms.TextBox txtMaintainUnit;
+        private System.Windows.Forms.Label lblMaintainUnit;
+        private System.Windows.Forms.Label lblOut;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ TadepalliS_BONUSAppartmentRental/TadepalliS_BONUSAppartmentRental/Form1.cs
@@ -0,0 +1,76 @@
+﻿/************************************************************
+    PROGRAMME : BONUS Apartment Rentals
+
+    OUTLINE : The programme prompts the user to input the number
+              of apartment units, the rent to occupy one of the 
+              units, the increase in rent that results in a vacant 
+              unit, and the amount to maintain a rented unit.The 
+              program then calculates the number of units to be 
+              rented in order to make the maximum profit.
+
+    PROGRAMMER : Saikrishna Tadepalli
+
+    DATE : January 6th 2020
+ ************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TadepalliS_BONUSAppartmentRental
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            if ((CheckDouble(txtAppartmentUnits) && CheckDouble(txtRentAll) && CheckDouble(txtRentIncrease) && CheckDouble(txtMaintainUnit)))
+            {
+                double appartmentsMax = Double.Parse(txtAppartmentUnits.Text);
+                double rentPerUnit = Double.Parse(txtRentAll.Text);
+                double rentIncrease = Double.Parse(txtRentIncrease.Text);
+                double maintainanceCost = Double.Parse(txtMaintainUnit.Text);
+
+                double rooms = Math.Round(((rentPerUnit - maintainanceCost + rentIncrease*appartmentsMax)/rentIncrease)/2);
+                double profit = (rooms) * (rentPerUnit - maintainanceCost + rentIncrease * (appartmentsMax - rooms));
+
+                lblOut.Text = "The number of units to rent to make the maximum profit of " + profit.ToString("C") + " is " + rooms + " units.";
+            }
+        }
+
+        private bool CheckDouble(TextBox t)
+        {
+            double outVar = new double();
+            string errorMessage = "Invalid Input!";
+
+            if (double.TryParse(t.Text, out outVar))
+                return true;
+            else
+                MessageBox.Show(errorMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            return false;
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
